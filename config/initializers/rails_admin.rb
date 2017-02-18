6d067edaa57f09f66496c7f1f329d98d3cccab3f
harim_kim <yongkal2@yonsei.ac.kr>
2017-02-18 05:26:06 +0000

    devise_railsadmin

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -4,9 +4,12 @@ RailsAdmin.config do |config|
 
   ## == Devise ==
   config.authenticate_with do
-    warden.authenticate! scope: :user
-  end
-  config.current_user_method(&:current_user)
+	    warden.authenticate! scope: :user
+	    if current_user.admin==false
+	      redirect_to main_app.root_path
+	    end
+	  end
+	config.current_user_method(&:current_user)
 
   ## == Cancan ==
   # config.authorize_with :cancan
