2ae1fb099c40e1d350c9d36d9cfa47e5980a8a78
EC2 Default User <ec2-user@ip-172-31-28-89.us-west-2.compute.internal>
2017-03-01 18:51:19 +0000

    google analitics, user.klass

--- db/migrate/20170210114415_devise_create_users.rb
+++ db/migrate/20170210114415_devise_create_users.rb
@@ -6,8 +6,9 @@ class DeviseCreateUsers < ActiveRecord::Migration
       t.string :encrypted_password, null: false, default: ""
       t.string :name
       t.string :major
+      t.string :klass
       t.boolean :admin
-
+     
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
